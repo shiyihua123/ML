864cc9c7df6619f6fce19b9ba2d262b02caf2b88
shiyihua123 <1960622591@qq.com>
2023-09-23 16:07:24 +0800

    Comparison of optimization methods

---
+++ code/NewtonLaw.py
@@ -0,0 +1,20 @@
+from random import Random
+import numpy as np
+def hx(x):
+    return x**2
+
+def x_grad(x):
+    return 2*x
+epochs = 1000
+# 牛顿法
+x0 = -0.5
+for e in range(epochs):
+    x0 = x0-(hx(x0)/x_grad(x0))
+print(x0)
+
+# 梯度下降法
+alpha = 1e-1
+x0 = -0.5
+for e in range(epochs):
+    x0 = x0-alpha*x_grad(x0)
+print(x0)
